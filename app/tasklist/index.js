73a98486b8d1455ee091f6f5e37f3e23ea9a2104
Kevin Growney <kevin@growney.net>
2015-01-25 06:32:47 -0800

    Added JS to scope

--- app/tasklist/index.js
+++ app/tasklist/index.js
@@ -1,4 +1,14 @@
 angular.module('tasklist', [
   /* Declare any module-specific dependencies here */
   'common'
-]);
+]).service('DateDiff',function () {
+  return {
+    calculateDays:function (date){
+      var ms= (1000 * 60 * 60 * 24);
+      var created_at = new Date(date);
+      var day_diff = Math.floor((new Date()- created_at) / ms);
+      return day_diff<7; //If not older than 7 days returns true and renders your task
+
+    }
+  }
+});
--- app/tasklist/scripts/IndexController.js
+++ app/tasklist/scripts/IndexController.js
@@ -1,17 +1,13 @@
 angular
   .module('tasklist')
-  .controller("IndexController", function ($scope, Tasklist, supersonic) {
+  .controller("IndexController", function ($scope, Tasklist, supersonic, DateDiff) {
     $scope.tasklists = null;
     $scope.showSpinner = true;
 
+
   var ms= (1000 * 60 * 60 * 24);
-  var now=new Date();
-        
- $scope.oldTask=function(task){
-        var created_at=task.created_at.split(':')[0];
-        var day_diff=Math.floor(created_at-current_date);
-        return (day_diff);
-    }
+  
+   
 
     Tasklist.all().whenChanged( function (tasklists) {
         $scope.$apply( function () {
